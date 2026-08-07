06130fa5acb1a5e622dfcddd10c0cdafab6d389f
j81d <77220086+j81d@users.noreply.github.com>
2026-08-07 00:00:24 -0600

    Add system monitor node to startup script

--- src/ros2_pi/start.sh
+++ src/ros2_pi/start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Source the ROS 2 Jazzy environment
 source /opt/ros/jazzy/setup.bash
 
 echo "Starting ROS 2 Nodes..."
 
-# Run GPIO node in the background
 python3 gpio_node.py &
 PID1=$!
 
-# Run Battery node in the background
 python3 canbat_node.py &
 PID2=$!
 
-# Keep the script running and wait for processes
-wait $PID1 $PID2
+# Add the new system monitor node
+python3 sys_monitor_node.py &
+PID3=$!
+
+wait $PID1 $PID2 $PID3
